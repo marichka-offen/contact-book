f0bffbb89ee0a666127cac367ca3a26c775a3c2e
Marichka Offen <40467112+marichka-offen@users.noreply.github.com>
2023-03-27 21:41:47 -0400

    Create searchbox component

---
+++ src/components/SearchBox.tsx
@@ -0,0 +1,23 @@
+import '../App.css'
+import { ChangeEventHandler } from 'react'
+
+type SearchBoxProps = {
+  className: string
+  placeholder?: string
+  onChangeHandler: ChangeEventHandler<HTMLInputElement>
+}
+
+const SearchBox = ({
+  className,
+  placeholder,
+  onChangeHandler,
+}: SearchBoxProps) => (
+  <input
+    className={`search-box ${className}`}
+    type='search'
+    placeholder={placeholder}
+    onChange={onChangeHandler}
+  />
+)
+
+export default SearchBox
